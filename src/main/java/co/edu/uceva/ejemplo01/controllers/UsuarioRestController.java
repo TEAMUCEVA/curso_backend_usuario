58a1dd77f465b6ba4bee6e9536b0a55918a91fab
Alvaro Salazar <alvaro@denkitronik.com>
2022-06-01 23:19:23 -0500

    Crud completo
    Signed-off-by: Alvaro Salazar <alvaro@denkitronik.com>

--- src/main/java/co/edu/uceva/ejemplo01/controllers/UsuarioRestController.java
+++ src/main/java/co/edu/uceva/ejemplo01/controllers/UsuarioRestController.java
@@ -17,30 +17,52 @@ public class UsuarioRestController {
     @Autowired
     private IUsuarioService usuarioService;
 
+    /**
+     * Endpoint de ejemplo que retorna un saludo
+     * @param nombre Es el nombre del usuario
+     * @return
+     */
     @GetMapping("/hola")
     public String saludar(@RequestParam(value = "nombre", defaultValue="Mundo") String nombre){
         return String.format("Hola %s", nombre);
     }
 
+    /**
+     * Metodo que devuelve la lista de usuarios
+     * @return Lista de usuarios
+     */
     @GetMapping("/usuarios")
     public List<Usuario> index(){
         return usuarioService.findAll();
     }
 
+    /**
+     * Metodo para buscar un usuario
+     * @param id Id del usuario a buscar
+     * @return El objeto usuario encontrado
+     */
     @GetMapping("/usuarios/{id}")
     public Usuario buscarUsuario(@PathVariable Long id){
         return usuarioService.findById(id);
     }
 
+    /**
+     * Metodo para crear un usuario
+     * @param usuario El usuario a persistir
+     * @return El usuario creado
+     */
     @PostMapping("/usuarios")
     @ResponseStatus(HttpStatus.CREATED)
     public Usuario crear(@RequestBody Usuario usuario){
         usuario.setCreadoEn(new Date());
         this.usuarioService.save(usuario);
-
         return usuario;
     }
 
+    /**
+     * Metodo que borra un usuario
+     * @param id Identificador del usuario
+     */
     @DeleteMapping("/usuarios/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void borrarUsuario(@PathVariable Long id){
@@ -48,6 +70,12 @@ public class UsuarioRestController {
         usuarioService.delete(usuario);
     }
 
+    /**
+     * Metodo para actualizar un usuario
+     * @param usuario Usuario modificado a persistir
+     * @param id Id del usuario a persistir
+     * @return El usuario modificado
+     */
     @PutMapping("/usuarios/{id}")
     @ResponseStatus(HttpStatus.CREATED)
     public Usuario actualizarUsuario(@RequestBody Usuario usuario, @PathVariable Long id){
@@ -59,7 +87,5 @@ public class UsuarioRestController {
         usuarioService.save(usuarioActual);
         return usuarioActual;
     }
-
-
 }
 
